f5eb3e2e1bce32d87d8d15a65dfd502e8177ff54
Simon Guest <me@simonguest.com>
2013-06-18 10:06:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+QTasks
+======
+
+A simple application and API, using Angular, Bootstrap, NodeJS, and Mongo, to support GTD-style task management
---
+++ app.js
@@ -0,0 +1,131 @@
+var express = require('express')
+    , http = require('http')
+    , path = require('path')
+    , mongoose = require('mongoose');
+
+var db = mongoose.createConnection('localhost', 'qtasks');
+
+var taskSchema = mongoose.Schema({description:'String', body:'String', project:'String', context:'String', thisweek:'Bool'});
+var taskModel = db.model('Task', taskSchema);
+
+//var registrationSchema = mongoose.Schema({firstname:'String', regdate:'String'});
+//var registrationModel = db.model('Registration', registrationSchema);
+
+var app = express();
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use('/public',express.static(path.join(__dirname,'/public')));
+    app.use('/templates',express.static(path.join(__dirname,'/templates')));
+
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+
+//app.get('/', routes.index);
+app.get('/', function(req,res){
+    res.sendfile('index.html');
+});
+
+/* list tasks */
+app.get('/api/tasks', function (req, res) {
+    return taskModel.find(function (err, tasks) {
+        if (!err) {
+            return res.send(tasks);
+        }
+        else {
+            return console.log(err);
+        }
+    });
+});
+
+/* list task by id*/
+app.get('/api/tasks/:id', function (req, res) {
+    return taskModel.findById(req.params.id, function (err, task) {
+        if (!err) {
+            return res.send(task);
+        }
+        else {
+            return console.log(err);
+        }
+    });
+});
+
+/* list registrations by task id */
+//app.get('/api/tournaments/:id/registrations', function (req, res) {
+//    return registrationModel.find({ "tournament_id" : req.params.id }, function (err, registrations) {
+//        if (!err) {
+//            return res.send(registrations);
+//        }
+//        else {
+//            return console.log(err);
+//        }
+//    });
+//});
+
+/* create */
+app.post('/api/tasks', function (req, res){
+    console.log("POST: ");
+    console.log(req.body);
+    var task = new taskModel({
+        description: req.body.description,
+        body: req.body.body,
+        project: req.body.project,
+        context: req.body.context,
+        thisweek: req.body.thisweek
+    });
+    task.save(function (err) {
+        if (!err) {
+            return console.log("created");
+        } else {
+            return console.log(err);
+        }
+    });
+    return res.send(task);
+});
+
+/* update */
+app.put('/api/tasks/:id', function (req, res){
+    return taskModel.findById(req.params.id, function (err, task) {
+        task.description = req.body.description;
+        task.body = req.body.body;
+        task.project = req.body.project;
+        task.context = req.body.context;
+        task.thisweek = req.body.thisweek;
+        return task.save(function (err) {
+            if (!err) {
+                console.log("updated");
+            } else {
+                console.log(err);
+            }
+            return res.send(task);
+        });
+    });
+});
+
+/* delete */
+app.delete('/api/tasks/:id', function (req, res){
+    return taskModel.findById(req.params.id, function (err, task) {
+        return task.remove(function (err) {
+            if (!err) {
+                console.log("removed");
+                return res.send('');
+            } else {
+                console.log(err);
+            }
+        });
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
