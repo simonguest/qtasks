6af9e4441276f2ab6b595f18dab09ea39e1ed091
Simon Guest <me@simonguest.com>
2013-07-01 20:01:53 -0700

    Completed oAuth for all CRUD methods

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express')
 
 var db = mongoose.createConnection('localhost', 'qtasks');
 
-var taskSchema = mongoose.Schema({description:'String', body:'String', project:'String', context:'String', thisweek:'Bool', fbid:'String'});
+var taskSchema = mongoose.Schema({description: 'String', body: 'String', project: 'String', context: 'String', thisweek: 'Bool', fbid: 'String'});
 var taskModel = db.model('Task', taskSchema);
 
 var app = express();
@@ -20,8 +20,8 @@ app.configure(function () {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use('/public',express.static(path.join(__dirname,'/public')));
-    app.use('/templates',express.static(path.join(__dirname,'/templates')));
+    app.use('/public', express.static(path.join(__dirname, '/public')));
+    app.use('/templates', express.static(path.join(__dirname, '/templates')));
 
 });
 
@@ -30,21 +30,20 @@ app.configure('development', function () {
 });
 
 //app.get('/', routes.index);
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-var getFacebookId = function(oAuthToken, callback) {
-    request("https://graph.facebook.com/me?fields=id&access_token="+oAuthToken, function(error, response, body){
+var getFacebookId = function (oAuthToken, callback) {
+    request("https://graph.facebook.com/me?fields=id&access_token=" + oAuthToken, function (error, response, body) {
         callback(JSON.parse(body).id);
     });
 }
 
 /* list tasks */
 app.get('/api/tasks', function (req, res) {
-    var token = req.headers['authorization'].substring(7);
-    getFacebookId(token, function(fbid){
-        return taskModel.find({"fbid":fbid}, function (err, tasks) {
+    getFacebookId(req.headers['authorization'].substring(7), function (fbid) {
+        return taskModel.find({"fbid": fbid}, function (err, tasks) {
             if (!err) {
                 return res.send(tasks);
             }
@@ -80,55 +79,64 @@ app.get('/api/tasks/:id', function (req, res) {
 //});
 
 /* create */
-app.post('/api/tasks', function (req, res){
-    console.log("POST: ");
-    console.log(req.body);
-    var task = new taskModel({
-        description: req.body.description,
-        body: req.body.body,
-        project: req.body.project,
-        context: req.body.context,
-        thisweek: req.body.thisweek
+app.post('/api/tasks', function (req, res) {
+    getFacebookId(req.headers['authorization'].substring(7), function (fbid) {
+        console.log("POST: ");
+        console.log(req.body);
+        var task = new taskModel({
+            description: req.body.description,
+            body: req.body.body,
+            project: req.body.project,
+            context: req.body.context,
+            thisweek: req.body.thisweek,
+            fbid: fbid
+        });
+        task.save(function (err) {
+            if (!err) {
+                return console.log("created");
+            } else {
+                return console.log(err);
+            }
+        });
+        return res.send(task);
     });
-    task.save(function (err) {
-        if (!err) {
-            return console.log("created");
-        } else {
-            return console.log(err);
-        }
-    });
-    return res.send(task);
 });
 
 /* update */
-app.put('/api/tasks/:id', function (req, res){
-    return taskModel.findById(req.params.id, function (err, task) {
-        task.description = req.body.description;
-        task.body = req.body.body;
-        task.project = req.body.project;
-        task.context = req.body.context;
-        task.thisweek = req.body.thisweek;
-        return task.save(function (err) {
-            if (!err) {
-                console.log("updated");
-            } else {
-                console.log(err);
-            }
-            return res.send(task);
+app.put('/api/tasks/:id', function (req, res) {
+    getFacebookId(req.headers['authorization'].substring(7), function (fbid) {
+        return taskModel.findOne({_id: req.params.id, fbid: fbid}, function (err, task) {
+            task.description = req.body.description;
+            task.body = req.body.body;
+            task.project = req.body.project;
+            task.context = req.body.context;
+            task.thisweek = req.body.thisweek;
+            task.fbid = fbid;
+
+            return task.save(function (err) {
+                if (!err) {
+                    console.log("updated");
+                } else {
+                    console.log(err);
+                }
+                return res.send(task);
+            });
         });
     });
 });
 
 /* delete */
-app.delete('/api/tasks/:id', function (req, res){
-    return taskModel.findById(req.params.id, function (err, task) {
-        return task.remove(function (err) {
-            if (!err) {
-                console.log("removed");
-                return res.send('');
-            } else {
-                console.log(err);
-            }
+app.delete('/api/tasks/:id', function (req, res) {
+    getFacebookId(req.headers['authorization'].substring(7), function (fbid) {
+        return taskModel.findOne({_id: req.params.id, fbid: fbid}, function (err, task) {
+            return task.remove(function (err) {
+                if (!err) {
+                    console.log("removed");
+                    return res.send('');
+                } else {
+                    console.log(err);
+                }
+            });
         });
     });
 });
