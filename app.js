5cccf837046eb558198d180df419e2a39c52235b
Simon Guest <me@simonguest.com>
2013-08-09 21:36:28 -0700

    Made call to FB's long live token to avoid nasty expiry issues every hour

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express')
     , http = require('http')
     , path = require('path')
     , mongoose = require('mongoose')
-    , request = require('request');
+    , request = require('request')
+    , url = require('url');
 
 var db = mongoose.createConnection('localhost', 'qtasks');
+
+var fb_appid = "530788920290681";
+var fb_appsecret = "028aeeb8614e458a23043a2dfd8bef52";
 
 var taskSchema = mongoose.Schema({description: 'String', body: 'String', project: 'String', context: 'String', thisweek: 'Bool', fbid: 'String'});
 var taskModel = db.model('Task', taskSchema);
@@ -34,8 +38,29 @@ app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
+app.get('/api/utils/tokenexchange/:token', function (req, res) {
+    getLongLivedToken(req.params.token, function (llToken) {
+        res.send(llToken);
+    })
+});
+
+function getParameterByName(bodytext, name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(bodytext);
+    return results == null ? "null" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function getLongLivedToken(oAuthToken, callback) {
+    console.log("obtaining long lived token for " + oAuthToken);
+    request("https://graph.facebook.com/oauth/access_token?client_id=" + fb_appid + "&client_secret=" + fb_appsecret + "&grant_type=fb_exchange_token&fb_exchange_token=" + oAuthToken, function (error, response, body) {
+        console.log(body);
+        callback(getParameterByName("?" + body, "access_token"));
+    });
+}
+
 var getFacebookId = function (oAuthToken, res, callback) {
-    console.log("token: "+oAuthToken);
+    console.log("token: " + oAuthToken);
     request("https://graph.facebook.com/me?fields=id&access_token=" + oAuthToken, function (error, response, body) {
 
         var result = JSON.parse(body);
@@ -43,17 +68,17 @@ var getFacebookId = function (oAuthToken, res, callback) {
         //var fakeError = '{"error":{"message":"Error validating access token: Session has expired at unix time 1374256800. The current unix time is 1374342424.","type":"OAuthException","code":190,"error_subcode":463}}';
         //var result  = JSON.parse(fakeError);
 
-        if (result.error)
-        {
-            console.log("Error: "+JSON.stringify(result));
+        if (result.error) {
+            console.log("Error: " + JSON.stringify(result));
             res.send({}, 401);
         }
-        else
-        {
-            console.log("calling back with "+result.id);
+        else {
+            console.log("calling back with " + result.id);
             callback(result.id);
         }
+
     });
+
 }
 
 /* list tasks */
